20ed622e6024ded78dff7116ab7459693626000b
Daniel Beck <daniel@beck.win>
2021-04-07 00:09:04 +0200

    infra: deploy pr

---
+++ packages/visual-tests/src/text-field.visual.spec.js
@@ -0,0 +1,12 @@
+describe('TextField', () => {
+  test.each([
+    ['standard'],
+  ])('%p', async variant => {
+    await global.page.goto(
+      `http://host.docker.internal:3123/iframe.html?id=components-textfield--${variant}&viewMode=story`
+    );
+    await page.waitForSelector('html.hydrated');
+    const previewHtml = await page.$('body');
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+  });
+});
